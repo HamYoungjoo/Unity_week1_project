0c165fe23f208c056422200e9b0f9cc3c3fc4009
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-09-07 10:45:06 +0900

    시간 표시 기능
    시간표시 기능 추가

---
+++ NewUnityProject/Assets/Scripts/TimeLabel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLabel : MonoBehaviour
+{
+
+    [SerializeField] Text time;
+
+    
+    void Update()
+    {
+        time.text = DateTime.Now.ToString("hh : mm");
+    }
+}
